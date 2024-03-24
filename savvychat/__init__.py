3843b492a4413e259180953ec08be9dab4f8c13b
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-24 11:52:15 +0530

    Update __init__.py

--- savvychat/__init__.py
+++ savvychat/__init__.py
@@ -22,7 +22,7 @@ mongo = MongoCli(config.MONGO_URL)
 db = mongo.Anonymous
 OWNER = config.OWNER_ID
 
-class nexichat(Client):
+class savvychat(Client):
     def __init__(self):
         super().__init__(
             name="savvychat",
