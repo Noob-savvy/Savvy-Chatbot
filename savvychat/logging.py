95630fa20334e6cc2bc7855b6e6ce1bf7cd3a346
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-24 09:17:38 +0530

    Update logging.py

--- savvychat/logging.py
+++ savvychat/logging.py
@@ -12,7 +12,6 @@ logging.basicConfig(
 
 logging.getLogger("httpx").setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
