918d05937108529d66f37458b3f458e8f29849ba
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-26 14:13:34 +0530

    Update blackbox.py

--- savvychat/modules/blackbox.py
+++ savvychat/modules/blackbox.py
@@ -11,24 +11,23 @@ from ..modules.helpers.inline import *
 
 x=None
 #blackbox
-@savvychat.on_message(filters.command(["blackbox"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
+@savvychat.on_message(filters.command(["blackbox"],  prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
 async def blackbox_chat(bot, message):
     if len(message.command) < 2:
-            await message.reply_text(
+        await message.reply_text(
             "Example:**\n\n`/blackbox write simple flask app code`")
     else:
         a = message.text.split(' ', 1)[1]
-    try:
-        response = requests.get(f'https://mukesh-api.vercel.app/blackbox?query={a}') 
-        if response.status_code==200:
-            await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-            x=response.json()["results"]
-            
-            await message.reply_text(f"{x}\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{savvychat.username} ",reply_markup=InlineKeyboardMarkup(GPT_BTN),quote=True,disable_web_page_preview =True)  
-        else:
-            pass
 
+        try:
+            response = requests.get(f'https://mukesh-api.vercel.app/blackbox?query={a}') 
+            if response.status_code == 200:
+                await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+                x = response.json()["results"]
             
-    except requests.exceptions.RequestException as e:
-        pass
-        
+                await message.reply_text(f"{x}\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{savvychat.username} ", reply_markup=InlineKeyboardMarkup(GPT_BTN), quote=True, disable_web_page_preview=True)  
+            else:
+                pass
+
+        except requests.exceptions.RequestException as e:
+            pass
