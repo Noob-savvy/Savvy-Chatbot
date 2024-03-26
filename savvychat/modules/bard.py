22022cf8da0476a95c7c943f054761cbaa7167ef
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-26 14:19:29 +0530

    Update bard.py

--- savvychat/modules/bard.py
+++ savvychat/modules/bard.py
@@ -31,3 +31,24 @@ async def bard_chat(bot, message):
     except requests.exceptions.RequestException as e:
         pass
         
+#========================================================bard=======================================#
+@savvychat.on_message(filters.command(["bard"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
+async def bard_chat(bot, message):
+    if len(message.command) < 2:
+        await message.reply_text(
+            "Example:**\n\n`/bard tell me about Python programming`")
+    else:
+        a = message.text.split(' ', 1)[1]
+
+        try:
+            response = requests.get(f'https://mukesh-api.vercel.app/bard?query={a}')
+            if response.status_code == 200:
+                await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
+                x = response.json()["results"]
+
+                await message.reply_text(f"{x}\n🎉ᴘᴏᴡᴇʀᴇᴅ ʙʏ @{savvychat.username} ", reply_markup=InlineKeyboardMarkup(GPT_BTN), quote=True)
+            else:
+                pass
+
+        except requests.exceptions.RequestException as e:
+            pass
