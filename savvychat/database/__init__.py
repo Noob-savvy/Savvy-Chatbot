64b0006fdbf5a584efb2d6c0bd46fc737263d7cf
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-27 20:36:01 +0530

    Update __init__.py

--- savvychat/database/__init__.py
+++ savvychat/database/__init__.py
@@ -2,9 +2,11 @@ from pymongo import MongoClient
 
 import config
 
-Savvydb = MongoClient(config.MONGO_DB_URL)
+Savvydb = MongoClient(config.MONGO_URL)
 Savvy = Savvydb["SavvyDb"]["Savvy"]
 
+Divudb = MongoClient(config.MONGO_DB_URL)
+Divu = Divudb["DivuDb"]["Divu"]
 
 from .chats import *
 from .users import *
