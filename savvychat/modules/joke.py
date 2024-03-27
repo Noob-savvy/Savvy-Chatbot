4b0043770f5b1800088a17222a8fa1a20334e26e
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-27 21:03:28 +0530

    Update joke.py

--- savvychat/modules/joke.py
+++ savvychat/modules/joke.py
@@ -6,10 +6,11 @@ JOKE_API_ENDPOINT = 'https://hindi-jokes-api.onrender.com/jokes?api_key=1a6d440e
 
 @app.on_message(filters.command("joke"))
 async def joke(_, message):
+   divy =await message.reply_text("👻")
     response = requests.get(JOKE_API_ENDPOINT)
     r = response.json()
     joke_text = r['jokeContent']
-    await message.reply_text(
-       f"✦ {joke_text} \n\n ❁ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [❀᭄ꦿ𝗟 𝗬 𝗞 𝗔 𝗔 [ᴬᶦ]](t.me/{app.username})\n❁ ᴍᴀᴅᴇ ʙʏ[𝕯𝖎𝖛𝖞𝖆𝖓𝖘𝖍𝖚 𝕽𝖆𝖓𝖆](t.me/about_ranavanshi_divy)"
+    await divy.edit_text(
+       f"✦ {joke_text} \n\n ❁ ᴘᴏᴡᴇʀᴇᴅ ʙʏ ➠ [❀᭄ꦿ𝗟 𝗬 𝗞 𝗔 𝗔 [ᴬᶦ]](t.me/{app.username})\n❁ ᴍᴀᴅᴇ ʙʏ ➠ [𝕯𝖎𝖛𝖞𝖆𝖓𝖘𝖍𝖚 𝕽𝖆𝖓𝖆](t.me/about_ranavanshi_divy)"
          )
             
