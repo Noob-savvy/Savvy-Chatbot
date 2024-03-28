fbefb33b9a3c10579c677c134620a5b8dd236e8b
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-03-28 21:33:43 +0530

    Add files via upload

---
+++ savvychat/__init__.py
@@ -0,0 +1,18 @@
+
+import time
+
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+import config
+from savvychat.logger import LOGGER
+
+
+boot = time.time()
+mongo = MongoCli(config.MONGO_DB_URL)
+db = mongo.Anonymous
+Mongo = MongoCli(config.MONGO_URL)
+Db = Mongo.divu
+OWNER = config.OWNER_ID
+LOGGER_ID = config.LOGGER_ID
+
+#bot client
+savvychat = savvychat()
