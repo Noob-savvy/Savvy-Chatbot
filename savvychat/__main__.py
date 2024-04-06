50c27d3ca087c3462b965768f09a893aea7fc8ed
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-04-06 11:45:03 +0530

    Update __main__.py

--- savvychat/__main__.py
+++ savvychat/__main__.py
@@ -11,6 +11,11 @@ from savvychat.modules import ALL_MODULES
 async def Savvy_boot():
     try:
         await savvychat.start()
+    except errors.exceptions.flood_420 as e:
+        wait_time = int(str(e).split("_")[2])
+        LOGGER.warning(f"Got FloodWait error, waiting for {wait_time} seconds...")
+        await asyncio.sleep(wait_time)
+        await Savvy_boot()
     except Exception as ex:
         LOGGER.error(ex)
         quit(1)
@@ -18,11 +23,10 @@ async def Savvy_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("savvychat.modules." + all_module)
        
-    LOGGER(__name__).info(f"Imported all modules successfully.......")
-    LOGGER(__name__).info(f"@{savvychat.username} is alive now..")
+    LOGGER.info("Imported all modules successfully.......")
+    LOGGER.info(f"@{savvychat.username} is alive now..")
     
     await idle()
-
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(Savvy_boot())
