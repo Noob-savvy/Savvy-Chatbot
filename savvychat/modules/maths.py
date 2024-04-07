677f6e37a58f7baff10bc234828d041d4d9ed564
Noob-savvy <151785835+Noob-savvy@users.noreply.github.com>
2024-04-07 08:17:57 +0530

    Create maths.py

---
+++ savvychat/modules/maths.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from savvychat import savvychat
+
+@savvychat.on_message(filters.command("math", prefixes="/"))
+def calculate_math(client, message):   
+    expression = message.text.split("/math ", 1)[1]
+    try:        
+        result = eval(expression)
+        response = f"ᴛʜᴇ ʀᴇsᴜʟᴛ ɪs : {result}"
+    except:
+        response = "ɪɴᴠᴀʟɪᴅ ᴇxᴘʀᴇssɪᴏɴ"
+    message.reply(response)
